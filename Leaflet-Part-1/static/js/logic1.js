1b6f677aee708265a0d3bd9180d909490b1a3c9e
cmswart <therabbitandtheox@gmail.com>
2023-08-16 19:55:50 -0400

    first commit

---
+++ Leaflet-Part-1/static/js/logic1.js
@@ -0,0 +1,43 @@
+console.log("Start of map using logic1");
+
+//logic1 creates initial tile layers, a layerGroup for earthquakes and layer control
+
+let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
+
+let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+// Create a baseMaps object.
+let baseMaps = {
+    "Street Map": street,
+    "Topographic Map": topo
+};
+
+// create an empty (new) leaflet layerGroup for earthquakes
+let earthquakes = new L.layerGroup();
+
+
+// Create an overlay object to hold our overlay.
+let overlayMaps = {
+    Earthquakes: earthquakes
+};
+
+// Create our map, giving it the streetmap and earthquakes layers to display on load.
+let myMap = L.map("map", {
+    center: [
+        37.09, -95.71
+    ],
+    zoom: 3.5,
+    layers: [street, earthquakes]
+    });
+
+
+// Create a layer control.
+// Pass it our baseMaps and overlayMaps.
+// Add the layer control to the map.
+L.control.layers(baseMaps, overlayMaps, {
+collapsed: false
+}).addTo(myMap);
